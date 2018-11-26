d7d2e875b85e99ea9a43eb6b3bf64cda40d0ea91
Clint Vidler <25560161+clintvidler@users.noreply.github.com>
2018-11-26 23:00:12 +1100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+// Enqueues the stylesheet of the parent theme
+
+<?php
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+function enqueue_parent_styles() {
+    wp_enqueue_style( 'parent-style', 
+    get_template_directory_uri().'/style.css' );
+}
+?>
